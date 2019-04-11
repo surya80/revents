bac0abf3a0ef2932296751421407903fdb138578
SuryaHS <surja.gain@gmail.com>
2019-04-11 08:38:44 +0530

    End of Section 5

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -8,7 +8,7 @@ const eventsDashboard = [
   {
     id: "1",
     title: "Trip to Tower of London",
-    date: "2018-03-27T11:00:00+00:00",
+    date: "2018-03-27",
     category: "culture",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
@@ -32,7 +32,7 @@ const eventsDashboard = [
   {
     id: "2",
     title: "Trip to Punch and Judy Pub",
-    date: "2018-03-28T14:00:00+00:00",
+    date: "2018-03-28",
     category: "drinks",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
@@ -61,7 +61,8 @@ class EventDashboard extends Component {
 
     this.state = {
       events: eventsDashboard,
-      isOpen: false
+      isOpen: false,
+      selectedEvent: null
     };
 
     // this.handleFormOpen = this.handleFormOpen.bind(this);
@@ -70,6 +71,7 @@ class EventDashboard extends Component {
 
   handleFormOpen = () => {
     this.setState({
+      selectedEvent: null,
       isOpen: true
     });
   };
@@ -79,6 +81,27 @@ class EventDashboard extends Component {
       isOpen: false
     });
   };
+
+  handleUpdateEvent = (updatedEvent) => {
+    this.setState({
+      events: this.state.events.map(event => {
+        if (event.id === updatedEvent.id) {
+          return Object.assign({}, updatedEvent);
+        } else {
+          return event
+        }
+      }),
+      isOpen: false,
+      selectedEvent: null
+    })
+  }
+
+  handleOpenEvent = (eventToOpen) => () => {
+    this.setState({
+      selectedEvent: eventToOpen,
+      isOpen: true
+    })
+  }
 
   handleCreateEvent = (newEvent) => {
     newEvent.id = cuid();
@@ -90,11 +113,20 @@ class EventDashboard extends Component {
     })
   };
 
+  handleDeleteEvent = (eventId) => () => {
+    const updatedEvents = this.state.events.filter(event => event.id !== eventId);
+    this.setState({
+      events: updatedEvents
+    })
+  }
+
   render() {
+    const {selectedEvent} = this.state;
+
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={this.state.events} />
+          <EventList  deleteEvent={this.handleDeleteEvent} onEventOpen={this.handleOpenEvent} events={this.state.events} />
         </Grid.Column>
         <Grid.Column width={6}>
           <Button
@@ -102,7 +134,8 @@ class EventDashboard extends Component {
             positive
             content="Create Event"
           />
-          {this.state.isOpen && <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />}
+          {this.state.isOpen && 
+          (<EventForm updateEvent={this.handleUpdateEvent} selectedEvent={selectedEvent} createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} /> )}
         </Grid.Column>
       </Grid>
     );
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -4,7 +4,7 @@ import EventListAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
   render() {
-    const {event} = this.props;
+    const {event, onEventOpen, deleteEvent} = this.props;
     return (
       <div style={{marginBottom: 10}}>
         <Segment.Group>
@@ -36,7 +36,8 @@ class EventListItem extends Component {
           </Segment>
           <Segment clearing>
             <span>{event.description}</span>
-            <Button as="a" color="teal" floated="right" content="View" />
+            <Button onClick={deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />
+            <Button onClick={onEventOpen(event)} as="a" color="teal" floated="right" content="View" />
           </Segment>
         </Segment.Group>
       </div>
